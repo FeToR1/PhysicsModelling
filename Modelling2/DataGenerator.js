02f28e26d4a42936377317f0cd6366fed01d90a3
FeToR <fedornigretuk@gmail.com>
2024-02-27 18:27:02 +0300

    fixed: lab2

--- Modelling2/DataGenerator.js
+++ Modelling2/DataGenerator.js
@@ -2,7 +2,7 @@ const nu0 = 1.2566370614 * 10e-6
 
 export function GenerateData(p) {
     let Positions = [[], []]
-    for (let i = -10 * p.R; i < 10 * p.R; i += 1) {
+    for (let i = -10 * p.R; i < 10 * p.R; i += 0.05) {
         Positions[0].push(i)
         Positions[1].push(p.N * nu0 * p.I * p.R * p.R / 2 * (1 / (i * i + p.R * p.R) ** 1.5 + 1 / ((i - p.R) ** 2 + p.R * p.R) ** 1.5))
     }
