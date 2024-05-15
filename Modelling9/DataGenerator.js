5af218f1b7a8796cc9b913ee81392affff788853
FeToR1 <fedornigretuk@gmail.com>
2024-05-15 20:42:45 +0300

    fixed: 9 lab

--- Modelling9/DataGenerator.js
+++ Modelling9/DataGenerator.js
@@ -2,15 +2,15 @@ function GenerateData(p) {
     let {lambda, n1, n2, d, L} = p
     lambda *= 1e-9
     d *= 1e-6
+    // console.log()
 
-    let Positions = [[], []]
-    const [num, delim] = [1000, 1]
+    let Positions = [[], [], L/d * lambda]
+    const [num, delim] = [1000, 1000]
+
     Positions[0] = Array.from({length: num}, (_, i) => i / delim)
     Positions[1] = Positions[0].map(x => Math.cos(n1 / n2 * d * x * Math.PI / lambda / L) ** 2 * 255) // Интенсивность
 
     return Positions
-
-
 }
 
 
@@ -20,41 +20,29 @@ export function DrawChart(p) {
     let data = []
     for (let i = 0; i < elements[0].length; i++) {
         data.push({
-            type: 'line', x0: elements[0][i], y0: 0, x1: elements[0][i], y1: 10, line: {
+            type: 'line', x0: elements[0][i] * 1000, y0: 0, x1: elements[0][i] * 1000, y1: 10, line: {
                 color: `rgb(${elements[1][i]},${elements[1][i]},${elements[1][i]})`,
             }
         })
     }
 
-    console.log(data)
-
-    var layout = {
-        title: 'Интерферреционные полосы', xaxis: {
-            range: [0, elements[0].length]
-        }, yaxis: {
-            range: [0, 10]
-        }, width: 1500, height: 500, shapes: data
+    const layout = {
+        title: 'Интерферреционные полосы',
+        xaxis: {
+            range: [0, elements[0].length],
+            // tickvals:
+            showticklabels: false
+        },
+        yaxis: {
+            range: [0, 10],
+            showticklabels: false
+        },
+        width: 1500,
+        height: 500,
+        shapes: data
     };
 
 
-    // let layout = []
-    // for (let i = 0; i < elements[0].length; i++) {
-    //     layout.push({
-    //         type: 'line',
-    //         x0: elements[0][i],
-    //         y0: 0,
-    //         x1: elements[0][i],
-    //         y1: elements[1][i],
-    //         line: {
-    //             color: 'rgb(55, 128, 191)',
-    //             width: 3
-    //         }
-    //     })
-    // }
-
-    console.log(layout)
-
     Plotly.newPlot('chart-container', [], layout);
 
-    // Plotly.newPlot('', data);
 }
